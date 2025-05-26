bb7e9524abc5492854e566a7e8dfe593ba303d9f
Pratham <pratham3407@gmail.com>
2025-05-27 02:35:13 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,35 +1,42 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js"; // on signup, login and logout auth.route.js file is  called
-import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
-import cors from "cors"; // cors is imported to enable cross-origin resource sharing
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import path from "path";
-import cookieParser from "cookie-parser"; // cookie-parser is imported to parse cookies
-import {app, server} from "./lib/socket.js"; // app is imported from socket.js file
-import {connectDB} from "./lib/db.js"; // connectDB function is imported from db.js file
-dotenv.config() 
-// dotenv is used to load environment variables from a .env file
 
+import { connectDB } from "./lib/db.js";
 
-const PORT = process.env.PORT
-const __dirname = path.resolve(); // __dirname is used to get the current directory name
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-app.use(cors({
-    origin: "http://localhost:5173", // frontend url
-    credentials: true, // allow credentials
-})); // cors is used to enable cross-origin resource sharing
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 app.use(express.json());
-app.use(cookieParser()); // cookie-parser is used to parse cookies
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist"))); // serve static files from the frontend build directory
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html")); // send index.html for all other routes
-    });
-} 
 
-server.listen(5001, () =>{
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+const routePath = "/*";
+ app.get(routePath, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+ });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,36 +1,37 @@
-import {Server} from 'socket.io';
-import http from 'http';
-import express from 'express';
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: ["http://localhost:5173"]
-    }
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
 });
 
-export function getReceiverSocketId(userId){
-    return userSocketMap[userId]; // returns the socketId of the user
-};
-// used to store online users
-const userSocketMap = {}; //{userid: socketid}
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
 
+// used to store online users
+const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
-    console.log("A user Connected", socket.id);
+  console.log("A user connected", socket.id);
 
-    const userId = socket.handshake.query.userId; // assuming userId is sent as a query parameter
-    if (userId) userSocketMap[userId] = socket.id; // store the userId and socketId in the map
-    // io.emit is used to emit an event to all connected clients
-    io.emit("getOnlineUsers", Object.keys(userSocketMap)); // emit the online users to all connected clients
-    
-    socket.on("disconnect", () => {
-        console.log("A user Disconnected", socket.id);
-        delete userSocketMap[userId]; // remove the userId from the map on disconnect    
-        io.emit("getOnlineUsers", Object.keys(userSocketMap)); // emit the updated online users to all connected clients
-    });
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
+
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
 });
 
-export { io, app, server }; 
+export { io, app, server };
